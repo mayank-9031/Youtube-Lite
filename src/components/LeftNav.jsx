5eabe659f417e8c90bb484799a76324b6e496722
Mayank <mayankgoyal.btprod20@pec.edu.in>
2023-05-03 02:14:16 +0530

    left category section

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -5,20 +5,39 @@ import { categories } from "../utils/constants";
 import { Context } from "../context/ContextApi";
 
 const LeftNav = () => {
-  const { selectedCategory, setSelectedCategory, mobileMenu } =
+  const { selectCatagories, setSelectCatagories, mobileMenu } =
     useContext(Context);
+
+  const navigate = useNavigate();
+
+  const clickHandler = (name, type) => {
+    switch (type) {
+      case "category":
+        return setSelectCatagories(name);
+      case "home":
+        return setSelectCatagories(name);
+      case "menu":
+        return false;
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240] md:translate-x-0 transition-all">
       <div className="flex px-5 flex-col">
         {categories.map((item) => {
           return (
-            <React.Fragment>
+            <React.Fragment key={item.name}>
               <LeftNavMenuItem
                 text={item.type === "home" ? "Home" : item.name}
                 icon={item.icon}
-                action={() => {}}
+                action={() => {
+                  clickHandler(item.name, item.type);
+                  navigate("/");
+                }}
                 className={`${
-                  selectedCategory === item.name ? "bg-white/[0.15]" : ""
+                  selectCatagories === item.name ? "bg-white/[0.15]" : ""
                 }`}
               />
               {item.divider && <hr className="my-5 border-white/[0.2]" />}
@@ -27,7 +46,7 @@ const LeftNav = () => {
         })}
         <hr className="my-5 border-white/[0.2]" />
         <div className="text-white/[0.5] text-[12px]">
-          Clone by: JS Dev Hindi
+          Developed by Mayank Goyal
         </div>
       </div>
     </div>
