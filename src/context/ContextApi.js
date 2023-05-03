9963ffbde408249ad500ca5bed02e68378c7ec3f
Mayank <mayankgoyal.btprod20@pec.edu.in>
2023-05-04 01:26:31 +0530

    videoCard section

--- src/context/ContextApi.js
+++ src/context/ContextApi.js
@@ -3,7 +3,7 @@ import { fetchDataFromApi } from "../utils/api";
 export const Context = createContext();
 export const AppContext = (props) => {
   const [loading, setLoading] = useState(false);
-  const [searchResults, setSearchResults] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
   const [selectCatagories, setSelectCatagories] = useState("New");
   const [mobileMenu, setMobileMenu] = useState(false);
 
