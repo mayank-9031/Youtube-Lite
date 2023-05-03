da5149d531f7cb5c3a122eb95924ac552b329890
Mayank <mayankgoyal.btprod20@pec.edu.in>
2023-05-04 02:03:03 +0530

    video details

--- src/components/SuggestResultVideo.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SuggestResultVideo = () => {
-  return <div>SuggestResultVideo</div>;
-};
-
-export default SuggestResultVideo;
---
+++ src/components/SuggestionVideoCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SuggestionVideoCard = () => {
+  return <div>videos</div>;
+};
+
+export default SuggestionVideoCard;
